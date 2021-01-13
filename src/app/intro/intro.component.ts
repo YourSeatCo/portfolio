99fbe3a6a3b708345a9fc75e1013260a346fb467
unknown <chad.smith@yourseat.co>
2021-01-13 10:09:45 -0500

    removed case studies to be added later

--- src/app/intro/intro.component.ts
+++ src/app/intro/intro.component.ts
@@ -13,3 +13,15 @@ export class IntroComponent implements OnInit {
   }
 
 }
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCzo_egIraZU2FdQ0P1uWTGoZKyRBcHg1A",
+    authDomain: "chadprototype.firebaseapp.com",
+    projectId: "chadprototype",
+    storageBucket: "chadprototype.appspot.com",
+    messagingSenderId: "632738783531",
+    appId: "1:632738783531:web:44819c3fd4072503d4656e"
+}
+}
